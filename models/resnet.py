a4b0f126076ae5b8d4d67825ff76b6a188758cb5
Felix Parker <flixpar@gmail.com>
2019-01-12 02:32:46 -0500

    implemented resnet+lstm model

--- models/resnet.py
+++ models/resnet.py
@@ -2,78 +2,35 @@ import torch
 from torch import nn
 import torchvision
 
-class Resnet(nn.Module):
-
-	def __init__(self, layers=152, n_classes=600, n_input_channels=3):
-		super(Resnet, self).__init__()
-
-		if layers == 152:
-			base_model = torchvision.models.resnet152(pretrained=True)
-		elif layers == 101:
-			base_model = torchvision.models.resnet101(pretrained=True)
-		elif layers == 50:
-			base_model = torchvision.models.resnet50(pretrained=True)
-		elif layers == 34:
-			base_model = torchvision.models.resnet34(pretrained=True)
-		else:
-			raise ValueError("Unsupported ResNet.")
-
-		if layers > 34: features_size = 2048
-		else: features_size = 512
-
-		conv1 = self.inflate_conv(base_model.conv1, n_input_channels)
-		self.resnet = nn.Sequential(
-			conv1,
-			base_model.bn1,
-			base_model.relu,
-			base_model.maxpool,
-			base_model.layer1,
-			base_model.layer2,
-			base_model.layer3,
-			base_model.layer4
-		)
-		self.pool = nn.AdaptiveAvgPool2d(1)
-		self.classifier = nn.Linear(features_size, n_classes)
-
-		nn.init.kaiming_normal_(self.classifier.weight, mode='fan_out', nonlinearity='relu')
-
-	def forward(self, x):
-		x = self.resnet(x)
-		x = self.pool(x)
-		x = x.view(x.size(0), -1)
-		x = self.classifier(x)
-		return x
-
-	def inflate_conv(self, layer, n_channels):
-
-		if n_channels == 3:
-			return layer
-
-		original_state_dict = layer.state_dict()
-		original_weights = original_state_dict["weight"]
-		s = original_weights.shape
-
-		mean_weights = original_weights.mean(dim=1).unsqueeze(dim=1)
-
-		if n_channels == 1:
-			weights = mean_weights
-		else:
-			weights = mean_weights.repeat(1, n_channels, 1, 1)
-
-		out_state_dict = original_state_dict
-		out_state_dict["weight"] = weights
-
-		out_layer = nn.Conv2d(n_channels, s[0], kernel_size=(s[2],s[3]),
-			stride=layer.stride, padding=layer.padding, bias=layer.bias)
-		out_layer.load_state_dict(out_state_dict)
-
-		return out_layer
+from models.modules import Resnet
 
 
 class ResNetLSTM(nn.Module):
 
-	def __init__(self):
-		pass
+	def __init__(self, n_classes=600, resnet_layers=101, hidden_size=1024, layers=2):
+		super(ResNetLSTM, self).__init__()
+		self.cnn = Resnet(layers=resnet_layers, output_dims=512)
+		self.lstm = nn.LSTM(
+			input_size = 512,
+			hidden_size = hidden_size,
+			num_layers = 2,
+			bidirectional = True,
+		)
+		self.fc = nn.Linear(hidden_size, n_classes)
+		self.statesize  = (layers*2, 1, hidden_size)
+		self.outputsize = 2 * hidden_size
+		self.state = self.init_state(self.statesize, None)
+		self.temporal_pooling = "mean"
 
 	def forward(self, x):
-		pass
+		x = self.cnn(x)
+		x = x.unsqueeze(0).permute(1, 0, 2)
+		output, _ = self.lstm(x, self.init_state(self.statesize, x.device))
+		if self.temporal_pooling == "mean": output = torch.mean(output, dim=0)
+		else: output = output[-1]
+		output = self.fc(output)
+		return output
+
+	def init_state(self, size, device):
+		return torch.zeros(size).to(device), torch.zeros(size).to(device)
+
